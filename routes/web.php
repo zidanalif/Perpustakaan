6114786727581f7992de2daa4392d2aea550e037
JihadAdhi <jihadadhirajasa@gmail.com>
2021-08-22 15:09:47 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,64 @@
 <?php
 
+
 /*
+
+
 |--------------------------------------------------------------------------
+
+
 | Web Routes
+
+
 |--------------------------------------------------------------------------
+
+
 |
+
+
 | Here is where you can register web routes for your application. These
+
+
 | routes are loaded by the RouteServiceProvider within a group which
+
+
 | contains the "web" middleware group. Now create something great!
+
+
 |
+
+
 */
 
+
 Route::get('/', function () {
-    return view('index');
+
+
+return view('index');
+
+
 });
+
+
+Route::resource('anggota','AnggotaController');
+
+
+Route::resource('kategori','KategoriController');
+
+
+Route::resource('buku','BukuController');
+
+
+Route::resource('transaksi','TransaksiController');
+
+
+Route::get('transaksi/edit/{id}', 'TransaksiController@edit');
+
+
+Route::get('transaksi/showBuku/{id}', 'TransaksiController@showBuku');
+
+
+Route::get('transaksi/getAnggota/{id}', 'TransaksiController@getAnggota');
+
+
+Route::post('/transaksi/update/{id}', 'TransaksiController@update');
