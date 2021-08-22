d664f24ea54fed9f084c4402a12f8f68a2845a20
Zidan Alif <zidanalifmaulana1@gmail.com>
2021-08-22 14:38:06 +0700

    view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+@section('title', 'Perpustakaan')
+@section('content')
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="display-4">Selamat Datang!</h1>
+        <p class="lead">Di Sistem Informasi Perpustakaan</p>
+        <hr class="my-4">
+    </div>
+</div>
+@endsection
